e013d265ff583110974355bedbb3eb30e6372904
0xrutvij <rutvij.shah96@gmail.com>
2023-04-27 19:20:16 -0500

    bump build ver.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="counterspeech",
-    version="0.3",
+    version="0.4",
     description="Auto counterspeech generation for hatespeech",
     packages=find_packages(where="."),
     include_package_data=True,
