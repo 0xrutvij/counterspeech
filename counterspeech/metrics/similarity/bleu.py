2fde5abc35e08d39d3bf78574f2ebfd1fb7bc290
0xrutvij <rutvij.shah96@gmail.com>
2023-04-27 22:33:31 -0500

    fix bleu score calculation logic & tokenizer download

--- counterspeech/metrics/similarity/bleu.py
+++ counterspeech/metrics/similarity/bleu.py
@@ -1,6 +1,6 @@
 from typing import final
 
-from nltk import word_tokenize
+from nltk import download, word_tokenize
 from nltk.translate.bleu_score import corpus_bleu
 
 from ..metric_base import Metric
@@ -14,6 +14,7 @@ class BLEU(Metric):
     def __init__(self, n_gram: int = 4) -> None:
         super().__init__()
         assert n_gram > 0 and n_gram < 5, "n_gram must be in [1, 4]"
+        download("punkt")
         self.n_gram = n_gram
 
     @property
@@ -28,7 +29,7 @@ class BLEU(Metric):
         ), "Length of predicted_labels and reference_labels must be equal"
 
         self.score = corpus_bleu(
-            [[word_tokenize(label) for label in reference_labels]],
+            [[word_tokenize(label)] for label in reference_labels],
             [word_tokenize(label) for label in predicted_labels],
             weights=[1 / self.n_gram for _ in range(self.n_gram)],
         )
