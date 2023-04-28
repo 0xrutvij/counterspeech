46369c88e4f17b2e208f1b1875c5e925c13983b4
0xrutvij <rutvij.shah96@gmail.com>
2023-04-28 15:54:27 -0500

    [WIP] bump ver.

--- counterspeech/models/__init__.py
+++ counterspeech/models/__init__.py
@@ -1,8 +1,5 @@
-from .bart_sparknlp import BARTonSpark
 from .dialo_gpt import DialoGPT
 from .gpt2 import GPT2
-from .gpt2_sparknlp import GPT2onSpark
-from .t5_sparknlp import T5onSpark
 
 __all__ = [
     "T5onSpark",
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="counterspeech",
-    version="0.4.7",
+    version="0.4.8",
     description="Auto counterspeech generation for hatespeech",
     packages=find_packages(where="."),
     include_package_data=True,
